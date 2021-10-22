972bfccd1be892cc53eef612f5a599653451a0b2
Tahabz <49519274+Tahabz@users.noreply.github.com>
2021-10-22 18:31:19 +0100

    add redis service

--- srcs/wordpress/tools/start.sh
+++ srcs/wordpress/tools/start.sh
@@ -4,8 +4,15 @@ wp core download --allow-root
 
 if [ ! -f "/www/wp-config.php" ]; then
 	wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost=$DB_HOST --path="/www" --dbprefix=wp_ --allow-root
+	wp config set WP_REDIS_HOST 'redis' --path="/www" --allow-root
+	wp config set WP_REDIS_PORT 6379 --path="/www" --allow-root
+	wp config set WP_REDIS_TIMEOUT 1 --path="/www" --allow-root
+	wp config set WP_REDIS_READ_TIMEOUT 1 --path="/www" --allow-root
+	wp config set WP_REDIS_DATABASE 0 --path="/www" --allow-root
 fi
 
 wp core install --url="10.11.44.241" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_EMAIL" --path="/www" --allow-root
+wp plugin install redis-cache --path="/www" --activate --allow-root
+cp /www/wp-content/plugins/redis-cache/includes/object-cache.php /www/wp-content
 
 php-fpm7.3 -F -R
