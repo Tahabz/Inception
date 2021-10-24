54794847cc49177a3443a8d4c38ae03cfd8fb51a
Tahabz <49519274+Tahabz@users.noreply.github.com>
2021-10-24 16:10:33 +0100

    add a localhost wpuser to mysql

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -8,6 +8,10 @@ service mysql start
 sed -i s/'127.0.0.1'/'0.0.0.0'/g /etc/mysql/mariadb.conf.d/50-server.cnf
 
 mysql -e "CREATE DATABASE wpdb;"
+mysql -e "CREATE USER 'wpuser'@'localhost' identified by '12345';"
+mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'%' IDENTIFIED BY '12345'"
+mysql -e "FLUSH PRIVILEGES;"
+
 mysql -e "CREATE USER 'wpuser'@'%' identified by '12345';"
 mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'%' IDENTIFIED BY '12345'"
 mysql -e "FLUSH PRIVILEGES;"
