b877fc4bba778b7ad83611560e2d17545a7cf42f
Tahabz <49519274+Tahabz@users.noreply.github.com>
2021-10-24 16:22:33 +0100

    use variables in mysql user and db creation

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -7,16 +7,16 @@ service mysql start
 
 sed -i s/'127.0.0.1'/'0.0.0.0'/g /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -e "CREATE DATABASE wpdb;"
-mysql -e "CREATE USER 'wpuser'@'localhost' identified by '12345';"
-mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'%' IDENTIFIED BY '12345'"
+mysql -e "CREATE DATABASE ${DB_NAME};"
+mysql -e "CREATE USER '${DB_USER}'@'localhost' identified by '${DB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysql -e "CREATE USER 'wpuser'@'%' identified by '12345';"
-mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'%' IDENTIFIED BY '12345'"
+mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysql wpdb < /wp.sql
+mysql ${DB_NAME} < /wp.sql
 
 service mysql stop
 
